f3c646c3e1e95b58e0f6bd018eb680f7822a94bb
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-09-03 15:00:46 +0300

    some commit

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
 from typing import Optional
-
+from pydantic import BaseModel,List
 
 app = FastAPI()
 
 @app.get("/ping")
 async def ping():
-    return {"message": "Bienvenue sur mon API FastAPI!"}
+    return "pong"
+
+class Cars(BaseModel):
+    id: str
+    brand: str
+    model: str
+    characteristics: dict = []
+
+cars_db:List = []
+@app.post("/cars",status_code=201)
+async def add_cars(cars:List[Cars]):
+    cars_db.extends(cars)
+    return cars_db
+
+
+
+@app.get("/cars")
+async def get_all_cars():
+    return cars_db
+
+@app.get("/cars/{id}")
+async def get_cars_by_id(find_id:int):
+    for()
+
